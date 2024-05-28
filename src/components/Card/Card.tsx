40f4bd0cbaea8fe359a430fcb31917be5335ad2d
BeatrizGuadalupeDiaz <beatrizdiaz13y19@gmail.com>
2024-05-28 12:59:27 -0600

    add new colors

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import {View, StyleSheet, Text, Image, Dimensions} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  Text,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
 import {useQuery} from '@tanstack/react-query';
 import {Colors} from './Colors';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const {height, width} = Dimensions.get('window');
 
 const Card = ({pokemon}: any) => {
+  const [isFavorite, setIsFavorite] = React.useState(false);
   const getPokemon = async ({queryKey}: any) => {
     const res = await fetch(queryKey[1]);
     return res.json();
@@ -15,13 +24,12 @@ const Card = ({pokemon}: any) => {
     ['getPokemon', pokemon?.url],
     getPokemon,
   );
+  const COLORS_POKEMON = Colors[data?.types[0]?.type?.name];
 
   return (
     <View
-      style={[
-        styles.container,
-        {backgroundColor: Colors[data?.types[0]?.type?.name]},
-      ]}>
+      style={[styles.container, {backgroundColor: COLORS_POKEMON}]}
+      key={data && data?.id}>
       <Text style={styles.name}>{data?.name}</Text>
       <Image
         style={styles.image}
@@ -30,13 +38,23 @@ const Card = ({pokemon}: any) => {
         }}
         resizeMode="contain"
       />
-      <View style={{alignItems: 'center'}}>
-        <Text style={styles.info}>
-          {data && (data?.id).toString().padStart(3, '0')}
-        </Text>
-        <Text style={styles.info}>
-          {data && (data?.types[0]?.type?.name).toString().padStart(3, '0')}
-        </Text>
+      <View style={styles.infoContainer}>
+        <View>
+          <Text style={[styles.info, {color: '#0969AF'}]}>
+            {data && (data?.id).toString().padStart(3, '0')}
+          </Text>
+          <Text style={[styles.info, {color: 'white'}]}>
+            {data && (data?.types[0]?.type?.name).toString().padStart(3, '0')}
+          </Text>
+        </View>
+        <TouchableOpacity
+          onPress={() => setIsFavorite(isFavorite ? false : true)}>
+          <Icon
+            name={'heart'}
+            size={35}
+            color={isFavorite ? '#F63639' : '#DBDBDA'}
+          />
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -45,7 +63,7 @@ const Card = ({pokemon}: any) => {
 const styles = StyleSheet.create({
   container: {
     width: '92%',
-    height: height * 0.4,
+    height: height * 0.3,
     marginVertical: 10,
     borderRadius: 20,
     alignItems: 'center',
@@ -67,12 +85,17 @@ const styles = StyleSheet.create({
   name: {
     fontSize: 20,
     fontWeight: 'bold',
-    color: 'black',
+    color: 'white',
   },
   info: {
     fontSize: 15,
     fontWeight: '800',
-    color: 'grey',
+    textAlign: 'center',
+  },
+  infoContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '60%',
   },
 });
 export default Card;
